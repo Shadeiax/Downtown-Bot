eca63ecaa018f76e1facb88cab541934dfe5de09
LAXCartMan <71773817+LAXCartMan@users.noreply.github.com>
2021-04-23 18:24:55 -0400

    Update Downtown-Bot.py

--- Downtown-Bot.py
+++ Downtown-Bot.py
@@ -23,25 +23,7 @@ async def ping(ctx):
 async def invite(ctx):     
     await ctx.send('The invite link to Relaxed Downtown is: https://discord.gg/NAhTTaaJuv')                         
 
-# VIP auto nickname
-<<<<<<< HEAD
-@client.command(pass_context=True)
-async def chnick(ctx, member: discord.Member, nick):
-    await member.edit(nick=nick)
-    await ctx.send(f'Nickname was changed for {member.mention} ')
-
-
-# async def on_member_update(before, after):
-#     if str(before.roel) == 'Level 9':
-#         if str(after.role) == "VIP's"
-
-
-
-
-client.run('ODM0ODQ2NTM2OTEwODk3MTkz.YIG1bQ.cnBtyNMeVTAJLfjwbmto7AJUvfg')
-=======
 @client.event
 async def on_member_update(before, after):
     
-client.run('TOKEN')
->>>>>>> 98b12eacd1e584575b6882ddd340b450164dc6f5
+client.run(TOKEN)
