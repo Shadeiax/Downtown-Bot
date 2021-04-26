458cb463725e5f63805dba4223c5e3d47b29bc56
LAXCartMan <cjweems311@gmail.com>
2021-04-26 15:54:55 -0400

    maybe work?  also have to do token

--- Downtown-Bot.py
+++ Downtown-Bot.py
@@ -13,22 +13,24 @@ async def on_ready():
 @client.command(aliases=['ms'])
 async def ping(ctx):
     await ctx.send(f'The bot has {round(client.latency * 1000)}ms ping.')
-
+    print('Ping/ms command has executed')
 # Invite link command
-@client.command()
-async def invite(ctx):     
-    await ctx.send('The invite link to Relaxed Downtown is: https://discord.gg/NAhTTaaJuv')                         
-
+@client.command(aliases=['link'])
+async def invite(ctx):
+    await ctx.send('The invite link to Relaxed Downtown is: https://discord.gg/NAhTTaaJuv')
+    print('Invite command has executed')
 @client.event
 async def on_member_update(before, after):
     role = after.guild.get_role(834942375377764352)
     if role in after.roles and role not in before.roles:
-        print(role, before.roles, after.roles)
-        after.edit(nick=f"🌟 {after.nick}")
+        after.edit(nick=f"(star emoji) {after.nick}")
+
+@client.command(pass_context=True)
+async def chnick(ctx, member: discord.Member, nick):
+        await member.edit(nick=nick)
 
 
 
 
 
-client.run('ODM0ODQ2NTM2OTEwODk3MTkz.YIG1bQ.vQjub7hnUbhXRAdzzrglXqBKvVs')
-
+client.run('TOKEN')
