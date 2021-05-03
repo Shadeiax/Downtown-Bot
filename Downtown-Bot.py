af693960738af12558251c69a110d3a71eef1a51
LAXCartMan <cjweems311@gmail.com>
2021-05-03 10:29:46 -0400

    oops forgot to commit, did a lot

--- Downtown-Bot.py
+++ Downtown-Bot.py
@@ -6,8 +6,11 @@ import asyncio
 import json
 import os
 
+intents = discord.Intents.default()
+intents.members = True
+
 # Bot prefix
-client = commands.Bot(command_prefix = ".")
+client = commands.Bot(command_prefix = ".", intents=intents)
 
 # Remove help
 client.remove_command("help")
@@ -18,13 +21,20 @@ async def on_ready():
     print('The bot is ready')
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=".help"))
 
-# # (RD Only) VIP role 🌟
-# @client.event
-# async def on_member_update(before, after, guild):
-#     role = after.guild.get_role(834942375377764352)
-#     print(role, before.roles, after.roles)
-#     if role in after.roles and role not in before.roles:
-#         await after.edit(nick=f"🌟 {after.nick}")
+# (RD Only) VIP role 🌟
+@client.event
+async def on_member_update(before, after):
+    role = after.guild.get_role(795321686517874698)
+    print(role, before.roles, after.roles, before.display_name, after.display_name)
+    if role in after.roles and role not in before.roles:
+        await after.edit(nick=f"🌟 {after.display_name}")
+# Tests
+@client.event
+async def on_member_update(before, after):
+    role = after.guild.get_role(837818932135067768)
+    print(role, before.roles, after.roles, before.display_name, after.display_name)
+    if role in after.roles and role not in before.roles:
+        await after.edit(nick=f"🌟 {after.display_name}")
 
 # Ping/ms command
 @client.command(aliases=['ms'])
@@ -148,9 +158,43 @@ async def coinflip(ctx, *, question):
     await ctx.send(embed=em)
     print('Coinflip command executed')
 
+# Dog command
+@client.command(aliases=['doge'])
+async def dog(ctx):
+    responses = [
+                    'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*',
+                    'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/dog_cool_summer_slideshow/1800x1200_dog_cool_summer_other.jpg',
+                    'https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d',
+                    'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg?crop=0.672xw:1.00xh;0.166xw,0&resize=640:*',
+                    'https://www.sciencemag.org/sites/default/files/styles/inline__450w__no_aspect/public/dogs_1280p_0.jpg?itok=h6VBayx-',
+                    'https://i.insider.com/5484d9d1eab8ea3017b17e29?width=600&format=jpeg&auto=webp'
+                                            ]
 
+    em = discord.Embed(title = "Dog", description="", color=discord.Colour.purple())
 
+    em.set_image(url=f"{random.choice(responses)}")
 
+    await ctx.send(embed=em)
+    print('Dog/doge command executed')
+
+# Amphibian command
+@client.command()
+async def lizard(ctx):
+    responses = [
+                    'https://cdn.discordapp.com/attachments/837804364990513183/838099013454397520/image0.jpg',
+                    'https://media.discordapp.net/attachments/837804364990513183/838099133189193728/image0.jpg?width=503&height=670',
+                    'https://animals.sandiegozoo.org/sites/default/files/2016-11/animals_hero_lizards.jpg',
+                    'https://images.unsplash.com/photo-1504450874802-0ba2bcd9b5ae?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGl6YXJkfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
+                    'https://cdn.britannica.com/37/125637-050-775F1B98/Agama.jpg',
+                    'https://static.scientificamerican.com/sciam/cache/file/35BB522C-1F80-4EA3-AD1A257F9B636908_source.jpg?w=590&h=800&47D6048D-D602-419B-B2B0A97B77D665FE'
+                                            ]
+
+    em = discord.Embed(title = "Lizard", description="", color=discord.Colour.purple())
+
+    em.set_image(url=f"{random.choice(responses)}")
+
+    await ctx.send(embed=em)
+    print('Lizard command executed')
 
 # Help command
 @client.group(invoke_without_command=True)
@@ -160,6 +204,7 @@ async def help(ctx):
 
     em.add_field(name="Moderation", value="kick, ban")
     em.add_field(name="Fun", value="8ball, coinflip" )
+    em.add_field(name="Images", value="dog, lizard" )
     em.add_field(name="Misc", value="invite, ping, about, workinprogress" )
     em.set_thumbnail(url="https://cdn.discordapp.com/avatars/834846536910897193/e8dd4276ccb10157dcf76f8222c9354c.png?size=256")
 
@@ -254,10 +299,31 @@ async def coinflip(ctx):
     await ctx.send(embed=em)
     print("Help coinflip/cf command has executed")
 
+# Help dog
+@help.command(aliases=['doge'])
+async def dog(ctx):
+    em = discord.Embed(title = "Dog", description="Displays a randomized dog image", color=discord.Colour.purple())
+
+    em.add_field(name="**Syntax**", value="`.dog/doge`")
+    em.set_thumbnail(url="https://cdn.discordapp.com/avatars/834846536910897193/e8dd4276ccb10157dcf76f8222c9354c.png?size=256")
+
+    await ctx.send(embed=em)
+    print("Help dog/doge command has executed")
+
+# Help amphibian
+@help.command()
+async def lizard(ctx):
+    em = discord.Embed(title = "Lizard", description="Displays a randomized lizard image", color=discord.Colour.purple())
+
+    em.add_field(name="**Syntax**", value="`.lizard`")
+    em.set_thumbnail(url="https://cdn.discordapp.com/avatars/834846536910897193/e8dd4276ccb10157dcf76f8222c9354c.png?size=256")
+
+    await ctx.send(embed=em)
+    print("Help lizard command has executed")
+
 # Suicide prevention
 # @client.event
-# async def on_message(message):
-#     ...
+# async def on_message(message, ctx):
 #     bad_words = ["suicide", "Suicide", "i want to kill myself", "i want to die", "I want to die", "I want to kill myself"]
 #
 #     for word in bad_words:
@@ -273,6 +339,9 @@ async def coinflip(ctx):
 #             em.set_thumbnail(url="https://cdn.discordapp.com/avatars/834846536910897193/e8dd4276ccb10157dcf76f8222c9354c.png?size=256")
 #
 #             await message.channel.send(embed=em)
-#     ...
+#
+#     else:
+#         return
+
 # Make the client run
 client.run('ODM0ODQ2NTM2OTEwODk3MTkz.YIG1bQ.VL1R-S7y2Vbqe9l8wpgCP9kQ8Pk')
