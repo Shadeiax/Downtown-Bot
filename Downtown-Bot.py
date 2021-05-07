986a18b831cf790aeefdef779b4dd2ae18e2198c
Carter <71773817+carter-py@users.noreply.github.com>
2021-05-07 08:36:18 -0400

    commits not working
    idk, manual upload without lofi.webm

--- Downtown-Bot.py
+++ Downtown-Bot.py
@@ -5,6 +5,9 @@ import random
 import asyncio
 import json
 import os
+import nacl
+import youtube_dl
+from discord import FFmpegPCMAudio
 
 intents = discord.Intents.default()
 intents.members = True
@@ -28,13 +31,14 @@ async def on_member_update(before, after):
     print(role, before.roles, after.roles, before.display_name, after.display_name)
     if role in after.roles and role not in before.roles:
         await after.edit(nick=f"🌟 {after.display_name}")
-# Tests
+
+# (RD Only) VIP auto DM 🌟
 @client.event
 async def on_member_update(before, after):
-    role = after.guild.get_role(837818932135067768)
+    role = after.guild.get_role(795321686517874698)
     print(role, before.roles, after.roles, before.display_name, after.display_name)
     if role in after.roles and role not in before.roles:
-        await after.edit(nick=f"🌟 {after.display_name}")
+        await after.send("```Hey! You! Yeah You! 😎\n\nLets go you just reached the VIP Status on Relaxed Downtown! 💜 \nAnd Yes we have some Premium features on Relaxed Downtown for VIP users: \n \n-Hidden VIP Chats ⭐ (You can find the VIP category under the Minigames category) \n \n-You can now request a nickname! (The text channel for that can be found in the Support category) 🦋\n \n-Question Of The Day :question: (Can be found in the Off-Topic Category) \n \n-Color Roles (can be found in the category User info) ```")
 
 # Ping/ms command
 @client.command(aliases=['ms'])
@@ -61,7 +65,7 @@ async def invite(ctx):
 async def kick(ctx, member : discord.Member, *, reason=None):
     await member.kick(reason=reason)
 
-    em = discord.Embed(title="Member Kicked", description=f"{member} was kick for {reason}", color=discord.Colour.purple())
+    em = discord.Embed(title="Member Kicked", description=f"{member} was kicked for {reason}", color=discord.Colour.purple())
 
     em.set_thumbnail(url="https://cdn.discordapp.com/avatars/834846536910897193/e8dd4276ccb10157dcf76f8222c9354c.png?size=256")
 
@@ -74,7 +78,7 @@ async def kick(ctx, member : discord.Member, *, reason=None):
 async def ban(ctx, member : discord.Member, *, reason=None):
     await member.ban(reason=reason)
 
-    em = discord.Embed(title="Member Banned", description=f"{member} was kick for {reason}", color=discord.Colour.purple())
+    em = discord.Embed(title="Member Banned", description=f"{member} was banned for {reason}", color=discord.Colour.purple())
 
     em.set_thumbnail(url="https://cdn.discordapp.com/avatars/834846536910897193/e8dd4276ccb10157dcf76f8222c9354c.png?size=256")
 
@@ -196,12 +200,117 @@ async def lizard(ctx):
     await ctx.send(embed=em)
     print('Lizard command executed')
 
-# # Playlist command
-# @client.command()
-# async def playlist(ctx):
-#     em = discord.Embed(title="Playlist", description="", color=discord.Colour.purple())
-#
-#     em.add_field(name='Spotify', value='')
+# Amphibian command
+@client.command()
+async def cat(ctx):
+    responses = [
+                    'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png',
+                    'https://c.files.bbci.co.uk/12A9B/production/_111434467_gettyimages-1143489763.jpg',
+                    'https://static.scientificamerican.com/sciam/cache/file/92E141F8-36E4-4331-BB2EE42AC8674DD3_source.jpg',
+                    'https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg',
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMkEG43ZrAEgJtx9uicyjBXIZNeAogYlNYYC9JCPMCC2yYj6_FptQC1uPYIVHaglnWy2E&usqp=CAU',
+                    'https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2020-07/kitten-510651.jpg?h=f54c7448&itok=ZhplzyJ9'
+                                            ]
+
+    em = discord.Embed(title = "Cat", description="", color=discord.Colour.purple())
+
+    em.set_image(url=f"{random.choice(responses)}")
+
+    await ctx.send(embed=em)
+    print('Cat command executed')
+
+# Playlist command
+@client.command()
+async def playlist(ctx):
+    em = discord.Embed(title="Playlist", description="", color=discord.Colour.purple())
+
+    em.add_field(name='Spotify', value='Listen to the offical Relaxed Downtown playlist on Spotify at: https://open.spotify.com/playlist/3VGaCebHKvTCnLNdcnglbB?si=b610100948354a72', inline=False)
+    em.add_field(name='Apple Music ', value='Listen to the offical Relaxed Downtown playlist on Apple Music at: https://music.apple.com/ch/playlist/favourite-songs-of-relaxed-downtown/pl.u-PDb44aEILVBdexv', inline=False)
+    await ctx.send(embed=em)
+    print('Playlist command executed')
+
+# Music stuff
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}   
+
+def endSong(guild, path):
+    os.remove(path)                                   
+
+@client.command(pass_context=True)
+async def play(ctx, url):
+    await ctx.send('Due to the file size of some YouTube videos, music can take up to 2 minutes to start playing')
+    if not ctx.message.author.voice:
+        await ctx.send('You are not connected to a voice channel') #message when you are not connected to any voice channel
+        return
+
+    else:
+        channel = ctx.message.author.voice.channel
+
+    voice_client = await channel.connect()
+
+    guild = ctx.message.guild
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        file = ydl.extract_info(url, download=True)
+        path = str(file['title']) + "-" + str(file['id'] + ".mp3")
+
+    voice_client.play(discord.FFmpegPCMAudio(path), after=lambda x: endSong(guild, path))
+    voice_client.source = discord.PCMVolumeTransformer(voice_client.source, 1)
+
+    await ctx.send(f'**Music: **{url}') #sends info about song playing right now
+
+@client.command(pass_context=True)
+async def lofi(ctx):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        voice = await channel.connect()
+        source = FFmpegPCMAudio('Lofi.webm')
+        player = voice.play(source)
+        await ctx.send(f"The bot is now playing lofi in {channel}")
+    else:
+        await ctx.send("You are not in a voice channel.  Join one to run the command!")
+
+@client.command()
+async def leave(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_connected():
+        await voice.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@client.command()
+async def pause(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("Currently no audio is playing.")
+
+
+@client.command()
+async def resume(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send("The audio is not paused.")
+
+
+@client.command()
+async def stop(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    voice.stop()
+
+
+
 
 # Help command
 @client.group(invoke_without_command=True)
@@ -212,7 +321,8 @@ async def help(ctx):
     em.add_field(name="Moderation", value="kick, ban")
     em.add_field(name="Fun", value="8ball, coinflip" )
     em.add_field(name="Images", value="dog, lizard" )
-    em.add_field(name="Misc", value="invite, ping, about, workinprogress" )
+    em.add_field(name="Misc", value="invite, ping, about, workinprogress, playlist" )
+    em.add_field(name="Music", value="play, lofi, pause, resume, stop" )
     em.set_thumbnail(url="https://cdn.discordapp.com/avatars/834846536910897193/e8dd4276ccb10157dcf76f8222c9354c.png?size=256")
 
     await ctx.send(embed=em)
@@ -327,6 +437,79 @@ async def lizard(ctx):
 
     await ctx.send(embed=em)
     print("Help lizard command has executed")
+
+# Help playlist
+@help.command()
+async def spotify(ctx):
+    em = discord.Embed(title = "Playlists", description="Displays the links for the offical Relaxed Downtown music!", color=discord.Colour.purple())
+
+    em.add_field(name="**Syntax**", value="`.playlist`")
+    em.set_thumbnail(url="https://cdn.discordapp.com/avatars/834846536910897193/e8dd4276ccb10157dcf76f8222c9354c.png?size=256")
+
+    await ctx.send(embed=em)
+    print("Help playlist command has executed")
+
+# Help music
+@help.command()
+async def play(ctx):
+    em = discord.Embed(title = "Play", description="Plays a specified YouTube video in the voice channel that you are connected to", color=discord.Colour.purple())
+
+    em.add_field(name="**Syntax**", value="`.play <url>`")
+    em.set_thumbnail(url="https://cdn.discordapp.com/avatars/834846536910897193/e8dd4276ccb10157dcf76f8222c9354c.png?size=256")
+
+    await ctx.send(embed=em)
+    print("Help play command has executed")
+
+@help.command()
+async def lofi(ctx):
+    em = discord.Embed(title = "Lofi", description="Plays a 2 hour long lofi flaylist in the voice channel that you are connected to", color=discord.Colour.purple())
+
+    em.add_field(name="**Syntax**", value="`.lofi`")
+    em.set_thumbnail(url="https://cdn.discordapp.com/avatars/834846536910897193/e8dd4276ccb10157dcf76f8222c9354c.png?size=256")
+
+    await ctx.send(embed=em)
+    print("Help lofi command has executed")
+
+@help.command()
+async def pause(ctx):
+    em = discord.Embed(title = "Pause", description="Pauses any currently playing music", color=discord.Colour.purple())
+
+    em.add_field(name="**Syntax**", value="`.pause`")
+    em.set_thumbnail(url="https://cdn.discordapp.com/avatars/834846536910897193/e8dd4276ccb10157dcf76f8222c9354c.png?size=256")
+
+    await ctx.send(embed=em)
+    print("Help pause command has executed")
+
+@help.command()
+async def resume(ctx):
+    em = discord.Embed(title = "Resume", description="Resumes any currently paused music", color=discord.Colour.purple())
+
+    em.add_field(name="**Syntax**", value="`.resume`")
+    em.set_thumbnail(url="https://cdn.discordapp.com/avatars/834846536910897193/e8dd4276ccb10157dcf76f8222c9354c.png?size=256")
+
+    await ctx.send(embed=em)
+    print("Help resume command has executed")
+
+@help.command()
+async def leave(ctx):
+    em = discord.Embed(title = "Leave", description="Disconnects the bot from the current voice channel", color=discord.Colour.purple())
+
+    em.add_field(name="**Syntax**", value="`.leave`")
+    em.set_thumbnail(url="https://cdn.discordapp.com/avatars/834846536910897193/e8dd4276ccb10157dcf76f8222c9354c.png?size=256")
+
+    await ctx.send(embed=em)
+    print("Help leave command has executed")
+
+@help.command()
+async def stop(ctx):
+    em = discord.Embed(title = "Stop", description="Stops the currently playing song and deletes it from the qeue", color=discord.Colour.purple())
+
+    em.add_field(name="**Syntax**", value="`.stop`")
+    em.set_thumbnail(url="https://cdn.discordapp.com/avatars/834846536910897193/e8dd4276ccb10157dcf76f8222c9354c.png?size=256")
+
+    await ctx.send(embed=em)
+    print("Help stop command has executed")
+
 
 # Suicide prevention
 # @client.event
